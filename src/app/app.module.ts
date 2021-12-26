c2070195acbb612991f871928b17e48fa14e65e8
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-12-26 19:46:33 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CounterComponent } from './counter/counter.component';
 import { counterReducer } from './state/counter.reducer';
+import { ShoppingCartComponent } from './shoppingCart/shoppingCart.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule
   , StoreModule.forRoot({ count:counterReducer }) ],
-  declarations: [ AppComponent, HelloComponent , CounterComponent ],
+  declarations: [ AppComponent, HelloComponent , CounterComponent,ShoppingCartComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/shoppingCart/shoppingCart.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-shopping-cart',
+  templateUrl: './shoppingCart.component.html',
+  styleUrls: ['./shoppingCart.component.css']
+})
+export class ShoppingCartComponent{}
